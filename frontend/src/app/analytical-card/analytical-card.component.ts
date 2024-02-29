2285b9381a5ed06da6e3bbf6b2e15ad3da4aa499
Buraym <BrayanWilis@outlook.com>
2024-02-29 16:44:23 -0300

    feat: add frontend init and analytical_card component

---
+++ frontend/src/app/analytical-card/analytical-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analytical-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './analytical-card.component.html',
+  styleUrl: './analytical-card.component.css'
+})
+export class AnalyticalCardComponent {
+
+}
